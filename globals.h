514aba4245a2c0ebc26bc48a2381e515c49c564f
marceloribeiro19 <158334382+marceloribeiro19@users.noreply.github.com>
2024-02-21 15:11:59 +0000

    Update globals.h
    The enum will be gone because now the tokens are defined in the Parser.y file

--- globals.h
+++ globals.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "test.tab.h"
 
 #ifndef FALSE
 #define FALSE 0
@@ -13,39 +14,7 @@
 #define TRUE 1
 #endif
 
-typedef enum
-{
-    /* book-keeping tokens */
-    ENDFILE, ERROR,
-
-    /* reserved words */
-    IF, ELSEIF, ELSE, DO, WHILE, FOR, SWITCH, CASE, DEFAULT, RETURN, BREAK,
-    INT, FLOAT, DOUBLE, CHAR, VOID, STRUCT, TYPEDEF,
-
-    /* multicharacter tokens */
-    NUM, ID,
-
-    /* special symbols */
-    ASSIGN, PLUS, MINUS, TIMES, OVER, PERCENT,LPAREN, RPAREN,LEFT_BRACE, 
-    RIGHT_BRACE, LEFT_BRACKET, RIGHT_BRACKET, TERNARY, SEMI, COLON, COMMA,
-
-    /* operators */
-    INCREMENT, DECREMENT, BITWISE_AND, BITWISE_OR, BITWISE_NOT, BITWISE_XOR,
-    LOGICAL_NOT, LOGICAL_AND, LOGICAL_OR, EQUAL, NOT_EQUAL, LESS_THAN, GREATER_THAN,
-    LESS_THAN_OR_EQUAL, GREATER_THAN_OR_EQUAL, RIGHT_SHIFT, LEFT_SHIFT, PLUS_ASSIGN, MINUS_ASSIGN,
-    MODULO, LEFT_SHIFT_ASSIGN, RIGHT_SHIFT_ASSIGN, BITWISE_AND_ASSIGN, BITWISE_OR_ASSIGN,
-    BITWISE_XOR_ASSIGN,
-
-    /* preprocessor and functions */
-    PREPROCESSOR, ARROW, DEFINE, EXIT,
-
-    /* additional tokens*/
-    CONSTANT, POINTER, ADDRESS_OF, 
-} TokenType;
-
-
 extern FILE* sourceCode;  /* source code text file */
-
-TokenType getToken(void);
+typedef int TokenType; 
 
 #endif
