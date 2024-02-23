9dc7488b45cfdba77a0946bf14449b4b31e6c00c
marceloribeiro19 <158334382+marceloribeiro19@users.noreply.github.com>
2024-02-23 12:20:31 +0000

    Modular main with --lex and --Parser modes
    to execute:
    $ ./test C_Program.txt
                  or
    $ ./test C_Program.txt --lex
                  or
    $ ./test C_Program.txt --parse

--- main.c
+++ main.c
@@ -5,17 +5,18 @@
 FILE * sourceCode;
 
 int main(int argc, char * argv[]){
-    
+    char* mode;     // --flex or --parse
     char pgm[120];
+
     /* 
-    * If the number of command-line arguments is not equal to 2,
+    * If the number of command-line arguments is not equal to 2 or 3,
     * print a usage message to stderr indicating the correct usage of the program,
-    * and exit the program with a return status of 1. (Correct usage: $ ./MyProgram test.c)
+    * and exit the program with a return status of 1. (Correct usage: $ ./MyProgram test.c or $ ./MyProgram test.c --lex/--parse)
     */
-    if (argc != 2){ 
-        fprintf(stderr,"usage: %s <filename>\n",argv[0]);
+    if (argc < 2 || argc > 3){  /* The mode is optional*/
+        fprintf(stderr,"usage: %s <filename> --<mode>\n",argv[0]);
         exit(1);
-    }
+    }else if (argc == 3) mode = argv[2];
     
     /* 
     * Copy the filename provided as a command-line argument to the 'pgm' variable.
@@ -23,7 +24,6 @@ int main(int argc, char * argv[]){
     */
     strcpy(pgm,argv[1]);
     if (strchr (pgm, '.') == NULL)  strcat(pgm,".c");
-
     /*Open the source code file in read mode and checks if it exists or cannot be opened*/
     sourceCode = fopen(pgm,"r");
     if (sourceCode==NULL)
@@ -31,10 +31,16 @@ int main(int argc, char * argv[]){
         fprintf(stderr,"File %s not found\n",pgm);
         exit(1);
     }
-
-    //while (getToken() != ENDFILE);  //We use this for a scanner-only compiler because the Parser(using bison) autommaticly fetches the tokens whe he needs them
-    parse();
     
+    if (strcmp(mode, "--lex") == 0) {
+        while (getToken() != ENDFILE);          //We use this for a scanner-only compiler because the Parser(using bison) autommaticly fetches the tokens whe he needs them
+    } else if (strcmp(mode, "--parse") == 0) {
+        parse();
+    } else {
+        printf("Default Mode\n");               //No mode was selected
+        return 0;
+    }
+  
     fclose(sourceCode); 
     return 0;
 }
